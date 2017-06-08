6faebadfbd9250505b7c74b39f471a3198abf265
Danny Smith <danny.j.smith@oracle.com>
2017-06-08 15:25:32 +0100

    First commit

---
+++ trumpbot.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+)
+
+func getResponseString() string {
+	responses := []string{
+		"It is my opinion that many of the leaks coming out of the White House are fabiricated lies made up by the #FakeNews media",
+		"This is the single greatest witch hunt of a politician in American history!",
+		"James Comey better hope that there are no 'tapes' of our conversations before he starts leaking to the press!",
+		"The Russia-Trump collusion story is a total hoax, when will this taxpayer funder charade end?",
+	}
+	return responses[rand.Intn(len(responses))]
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s\n", getResponseString())
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8081", nil)
+}
